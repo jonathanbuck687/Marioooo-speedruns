ada9b19ce03832351e9128b71e54d40cf009d5ad
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-05-01 11:24:19 -0600

    mariooo

--- src/DataSetUtility.java
+++ src/DataSetUtility.java
@@ -44,13 +44,30 @@ public class DataSetUtility {
     }
     public String bestCountryPlacement(String country) {
         int best = 1000;
+        String bestCountry = "";
         for (int i = 0; i < mariooo.size(); i++) {
             if(mariooo.get(i).getCountry().equals(country)) {
                 if (mariooo.get(i).getPlacement() < best) {
                     best = mariooo.get(i).getPlacement();
+                    bestCountry = mariooo.get(i).getCountry();
+                }
+            }
+        }
+        return bestCountry;
+    }
+    public int personalRecord(String name) {
+        int best = Integer.MAX_VALUE;
+        for (int i = 0; i < mariooo.size(); i++) {
+            if(mariooo.get(i).getName().equals(name)) {
+                if (mariooo.get(i).getTime() < best) {
+                    best = mariooo.get(i).getTime();
                 }
             }
         }
         return best;
     }
+    public String getNamePlacement(int placement) {
+        return mariooo.get(placement - 1).getName();
+    }
+
 }
